252a74ee600b9ec17a66868bef261d1be40dd933
alix babin <alix.babin69@gmail.com>
2024-02-23 16:29:11 +0100

    tests unitaires

--- src/test/java/com/ipi/jva350/model/EntrepriseTest.java
+++ src/test/java/com/ipi/jva350/model/EntrepriseTest.java
@@ -10,49 +10,128 @@ class EntrepriseTest {
 
     @Test
     public void testDateDansLaPlage() {
+        //GIVEN
         LocalDate d = LocalDate.of(2022, 3, 15);
         LocalDate debut = LocalDate.of(2022, 3, 1);
         LocalDate fin = LocalDate.of(2022, 3, 31);
-        assertTrue(Entreprise.estDansPlage(d, debut, fin));
+        //WHEN
+        boolean estDansPlage = Entreprise.estDansPlage(d, debut, fin);
+        //THEN
+        assertTrue(estDansPlage);
     }
 
     @Test
     public void testDateAvantLaPlage() {
+        //GIVEN
         LocalDate d = LocalDate.of(2022, 2, 28);
         LocalDate debut = LocalDate.of(2022, 3, 1);
         LocalDate fin = LocalDate.of(2022, 3, 31);
-        assertFalse(Entreprise.estDansPlage(d, debut, fin));
+        //WHEN
+        boolean estDansPlage = Entreprise.estDansPlage(d, debut, fin);
+        //THEN
+        assertFalse(estDansPlage);
     }
 
     @Test
     public void testDateApresLaPlage() {
+        //GIVEN
         LocalDate d = LocalDate.of(2022, 4, 1);
         LocalDate debut = LocalDate.of(2022, 3, 1);
         LocalDate fin = LocalDate.of(2022, 3, 31);
-        assertFalse(Entreprise.estDansPlage(d, debut, fin));
+        //WHEN
+        boolean estDansPlage = Entreprise.estDansPlage(d, debut, fin);
+        //THEN
+        assertFalse(estDansPlage);
     }
 
     @Test()
     public void testDateNulle() {
+        //GIVEN
         LocalDate d = null;
         LocalDate debut = LocalDate.of(2022, 3, 1);
         LocalDate fin = LocalDate.of(2022, 3, 31);
+        //WHEN and THEN
         assertThrows(IllegalArgumentException.class, () -> Entreprise.estDansPlage(d, debut, fin));
     }
 
     @Test()
     public void testDebutNulle() {
+        //GIVEN
         LocalDate d = LocalDate.of(2022, 3, 1);
         LocalDate debut = null;
         LocalDate fin = LocalDate.of(2022, 3, 31);
+        //WHEN and THEN
         assertThrows(IllegalArgumentException.class, () -> Entreprise.estDansPlage(d, debut, fin));
     }
 
     @Test()
     public void testFinNulle() {
+        //GIVEN
         LocalDate d = LocalDate.of(2022, 3, 5);
         LocalDate debut = LocalDate.of(2022, 3, 1);
         LocalDate fin = null;
+        //WHEN and THEN
         assertThrows(IllegalArgumentException.class, () -> Entreprise.estDansPlage(d, debut, fin));
+    }
+    //tests get premier jour annee de conges
+    @Test
+    public void testDateNull() {
+        // GIVEN
+        LocalDate date = null;
+
+        // WHEN
+        LocalDate premierJourAnneeDeConges = Entreprise.getPremierJourAnneeDeConges(date);
+
+        // THEN
+        assertNull(premierJourAnneeDeConges);
+    }
+
+    @Test
+    public void testDateApresPremierJuin() {
+        // GIVEN
+        LocalDate date = LocalDate.of(2022, 6, 15); // Un exemple de date après le 1er juin
+
+        // WHEN
+        LocalDate premierJourAnneeDeConges = Entreprise.getPremierJourAnneeDeConges(date);
+
+        // THEN
+        assertEquals(LocalDate.of(2022, 6, 1), premierJourAnneeDeConges);
+    }
+
+    @Test
+    public void testDateAvantPremierJuin() {
+        // GIVEN
+        LocalDate date = LocalDate.of(2022, 5, 31); // Un exemple de date juste avant le 1er juin
+
+        // WHEN
+        LocalDate premierJourAnneeDeConges = Entreprise.getPremierJourAnneeDeConges(date);
+
+        // THEN
+        assertEquals(LocalDate.of(2021, 6, 1), premierJourAnneeDeConges);
+    }
+
+    //tests est jour ferié
+    @Test
+    public void testJourFerie() {
+        // GIVEN
+        LocalDate jourFerieConnu = LocalDate.of(2022, 1, 1);
+
+        // WHEN
+        boolean estFerie = Entreprise.estJourFerie(jourFerieConnu);
+
+        // THEN
+        assertTrue(estFerie);
+    }
+
+    @Test
+    public void testJourNonFerie() {
+        // GIVEN
+        LocalDate jourOrdinaire = LocalDate.of(2022, 2, 28);
+
+        // WHEN
+        boolean estFerie = Entreprise.estJourFerie(jourOrdinaire);
+
+        // THEN
+        assertFalse(estFerie);
     }
 }
--- src/test/java/com/ipi/jva350/service/SalarieAideADomicileServiceTest.java
+++ src/test/java/com/ipi/jva350/service/SalarieAideADomicileServiceTest.java
@@ -3,7 +3,6 @@ package com.ipi.jva350.service;
 import com.ipi.jva350.exception.SalarieException;
 import com.ipi.jva350.model.SalarieAideADomicile;
 import com.ipi.jva350.repository.SalarieAideADomicileRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -13,6 +12,10 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class SalarieAideADomicileServiceTest {
     @Mock
@@ -33,7 +36,23 @@ class SalarieAideADomicileServiceTest {
         // Then :
         ArgumentCaptor<SalarieAideADomicile> salarieAideADomicileCaptor = ArgumentCaptor.forClass(SalarieAideADomicile.class);
         Mockito.verify(salarieAideADomicileRepository, Mockito.times(1)).save(salarieAideADomicileCaptor.capture()); // arg capture !
-        Assertions.assertEquals(1L, salarieAideADomicileCaptor.getValue().getCongesPayesPrisAnneeNMoins1());
+        assertEquals(1L, salarieAideADomicileCaptor.getValue().getCongesPayesPrisAnneeNMoins1());
+    }
+
+    @Test
+    void testCalculeLimiteEntrepriseCongesPermis() {
+        // Given
+        LocalDate moisEnCours = LocalDate.of(2022, 3, 1);
+        double congesPayesAcquisAnneeNMoins1 = 20;
+        LocalDate moisDebutContrat = LocalDate.of(2020, 1, 1);
+        LocalDate premierJourDeConge = LocalDate.of(2022, 7, 1);
+        LocalDate dernierJourDeConge = LocalDate.of(2022, 7, 10);
+        when(salarieAideADomicileRepository.partCongesPrisTotauxAnneeNMoins1()).thenReturn(0.5);
+        // When
+        long limiteConges = this.salarieAideADomicileService.calculeLimiteEntrepriseCongesPermis(moisEnCours, congesPayesAcquisAnneeNMoins1, moisDebutContrat, premierJourDeConge, dernierJourDeConge);
+        // Then
+        long expectedLimiteConges = 5;
+        assertEquals(expectedLimiteConges, limiteConges);
     }
 
 //    @Test
